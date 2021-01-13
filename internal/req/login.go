bd2cb9d20aeae7be0e75f9e70943755e2b3d764d
Lucas Santiago <lu.santi.oli@gmail.com>
2021-01-13 15:34:30 -0300

    feat(login): removing unnecessary login requeriments

--- internal/req/login.go
+++ internal/req/login.go
@@ -8,31 +8,19 @@ import (
 
 // Login A struct that holds the login information
 type Login struct {
-	BasicAuthUserName  string
-	BasicAuthPassword  string
 	BasicSecretAuthKey string
 	Request            *Request
 }
 
 // NewMundipagg Create a Mundipagg object
-func NewMundipagg(basicAuthUserName string, basicAuthPassword string, basicSecretAuthKey string) (*Login, error) {
+func NewMundipagg(basicSecretAuthKey string) (*Login, error) {
 	// Verify parameters
-	if utils.IsStringEmpty(basicAuthUserName) {
-		return nil, errors.New("Please provide basicAuthUserName")
-	}
-
-	if utils.IsStringEmpty(basicAuthPassword) {
-		return nil, errors.New("Pleease provide basicAuthPassword")
-	}
-
 	if utils.IsStringEmpty(basicSecretAuthKey) {
 		return nil, errors.New("Pleease provide basicSecretAuthKey")
 	}
 
 	// Create the mundipagg object
 	login := &Login{
-		BasicAuthUserName:  basicAuthUserName,
-		BasicAuthPassword:  basicAuthPassword,
 		BasicSecretAuthKey: basicSecretAuthKey,
 	}
 
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package mundipagg
 import "github.com/lusantisuper/mundipagg/internal/req"
 
 // NewMundipagg Create a Mundipagg object
-func NewMundipagg(basicAuthUserName string, basicAuthPassword string, basicSecretAuthKey string) (*req.Login, error) {
-	return req.NewMundipagg(basicAuthUserName, basicAuthPassword, basicSecretAuthKey)
+func NewMundipagg(basicSecretAuthKey string) (*req.Login, error) {
+	return req.NewMundipagg(basicSecretAuthKey)
 }
 
 func main() {}
