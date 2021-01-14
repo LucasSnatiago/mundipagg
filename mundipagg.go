e58fb28740a2d181350986349652b2b5d403ee5e
Lucas Santiago <lu.santi.oli@gmail.com>
2021-01-14 16:41:59 -0300

    fix: interface

--- mundipagg.go
+++ mundipagg.go
@@ -3,7 +3,7 @@ package mundipagg
 // Mundipagg struct that holds all functions working in the lib
 type Mundipagg interface {
 	NewSubscription(s Subscription, indepotencyKey string) (*Response, error)
-	NewCustomer(c Customer, indepotencyKey string) (string, error)
+	NewCustomer(c Customer, indepotencyKey string) (*Response, error)
 }
 
 // Login A struct that holds the login information
