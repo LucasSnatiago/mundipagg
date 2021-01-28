165e0b1ff98cee0db18b358782786444b95e832c
Lucas Santiago <lu.santi.oli@gmail.com>
2021-01-28 12:57:33 -0300

    feat: adding AddDiscount to the interface

--- mundipagg.go
+++ mundipagg.go
@@ -7,6 +7,7 @@ type Mundipagg interface {
 	NewCardByToken(customerID string, cardToken string, indepotencyKey string) (*Response, error)
 	UpdateStartAt(u *UpdateStartAtSubscription, mundipaggCustomerID string, indepotencyKey string) (*Response, error)
 	UpdateNextBillingDay(u *UpdateNextBillingDaySubscription, mundipaggCustomerID string, indepotencyKey string) (*Response, error)
+	AddDiscount(b *BillExtras, mundipaggSubscriptionID string, indepotencyKey string) (*Response, error)
 }
 
 // Login A struct that holds the login information
